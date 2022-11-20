cc3be649b888e62c89acb6734d1f4f628a07d5ae
mohamed-imthiyaj <mohamedimthiyaj.sadhikbatcha@ainqa.com>
2022-11-20 19:24:10 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import { actions } from "ems-databinder";
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import "./App.css";
 
 const App = () => {
   const dispatch = useDispatch();
-  let Vital_workspace = useSelector(
-    (state) => state?.patientSlice?.read_db?.data?.result?.[0]
+  let getReadDoc = useSelector(
+    (state) => state?.patientSlice?.read_document?.data?.result
   );
-  const getPatientData = async () => {
-    await dispatch(actions.READ_DB());
+  const getReadDocument = async () => {
+    await dispatch(actions.READ_DOCUMENT());
   };
   useEffect(() => {
-    getPatientData();
+    getReadDocument();
   }, []);
-  console.log(Vital_workspace);
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Home</h1>
-        <h1>AGE :{Vital_workspace?.age}</h1>
+        <h1>ReadDocument</h1>
+        <table>
+          <tr>
+            <th>_key</th>
+            <th>_id</th>
+            <th>Name</th>
+          </tr>
+          {getReadDoc?.map((getdata) => (
+            <>
+              <tr>
+                <td>{getdata._key}</td>
+                <td>{getdata._id}</td>
+                <td>{getdata?.name}</td>
+              </tr>
+            </>
+          ))}
+        </table>
       </header>
     </div>
   );
